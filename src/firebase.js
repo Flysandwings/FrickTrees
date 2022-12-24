e551af84b117f42621cc5d8059f6cb7de8e057bb
Flysandwings <az19agent@gmail.com>
2022-12-24 12:06:47 -0500

    Delete firebase.js

--- src/firebase.js
+++
@@ -1,9 +0,0 @@
-import { initializeApp } from "firebase/app";
-import { getDatabase } from "firebase/database";
-
-const firebaseConfig = {
-  contactme
-};
-
-const app = initializeApp(firebaseConfig);
-export const db = getDatabase(app);
