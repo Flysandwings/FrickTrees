1b2dbe049f304ea3e981e2832ec848ca6382cf69
Flysandwings <az19agent@gmail.com>
2022-12-24 12:05:34 -0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAetL7GJNnVRz_FVyxakGBOBVCkxs1sZms",
-  authDomain: "pdgajr.firebaseapp.com",
-  projectId: "pdgajr",
-  storageBucket: "pdgajr.appspot.com",
-  messagingSenderId: "312194055374",
-  appId: "1:312194055374:web:5780b1cb470ffaab29249f",
-  databaseURL: "https://pdgajr-default-rtdb.firebaseio.com/",
+  contactme
 };
 
 const app = initializeApp(firebaseConfig);
