4e26d8fe16406eaf93b3f86e33b0208fe07ec882
Flysandwings <az19agent@gmail.com>
2023-02-15 20:23:46 -0500

    Commit

--- src/firebase.js
+++ src/firebase.js
@@ -2,7 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  *** REDACTED ***
+  apiKey: "AIzaSyAetL7GJNnVRz_FVyxakGBOBVCkxs1sZms",
+  authDomain: "pdgajr.firebaseapp.com",
+  projectId: "pdgajr",
+  storageBucket: "pdgajr.appspot.com",
+  messagingSenderId: "312194055374",
+  appId: "1:312194055374:web:5780b1cb470ffaab29249f",
+  databaseURL: "https://pdgajr-default-rtdb.firebaseio.com/",
 };
 
 const app = initializeApp(firebaseConfig);
